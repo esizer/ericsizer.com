d521a273dafa11aa37d9b6a017ded43af974e756
Eric Sizer <eric.sizer@gmail.com>
2021-04-30 17:33:09 -0400

    update meta

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: "/",
-  title: "Floating-Point Starter",
-  tagline: "A Basic Starter",
-  description: "Basic starter for Floating-Point developer Gatsby sites",
+  title: "Eric Sizer",
+  tagline: "A Web Developer",
+  description: "Eric Sizer - A Web Developer",
   url: "http://localhost:3000", // Domain of your site. No trailing slash!
   siteLanguage: "en",
   logo: "src/images/logo.png", // Used for SEO
@@ -10,11 +10,11 @@ module.exports = {
   /**
    * JSONLD Attributes
    */
-  titleAlt: "Floating-Point Starterr", // Title for JSONLD
-  headline: "Basic starter for Floating-Point developer Gatsby sites", // Headline for schema.org JSONLD
+  titleAlt: "Eric Sizer", // Title for JSONLD
+  headline: "A Web Developer", // Headline for schema.org JSONLD
   favicon: "src/images/favicon.png", // Used for manifest favicon generation
-  shortName: "FP", // shortname for manifest. MUST be shorter than 12 characters
-  author: "Floating-Point", // Author for schemaORGJSONLD
+  shortName: "ES", // shortname for manifest. MUST be shorter than 12 characters
+  author: "Eric Sizer", // Author for schemaORGJSONLD
   themeColor: "#6366F1",
   backgroundColor: "#6366F1",
 
@@ -22,8 +22,8 @@ module.exports = {
    * Social Media
    */
   ogLanguage: "en_CA", // Facebook Language
-  twitter: "@floatingpointco", // Twitter Username
-  facebook: "Floating-Point",
+  twitter: "@irtaco", // Twitter Username
+  facebook: "Eric Sizer",
 
   /**
    * Skip Nav
